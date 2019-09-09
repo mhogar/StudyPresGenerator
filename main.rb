43042c8439b5f306b5f7cc40fcedcd0b9c6568ff
Ryan <mhogar98@gmail.com>
2019-09-09 19:01:25 -0400

    Now randomizing order of questions + added latex compiler system call

--- main.rb
+++ main.rb
@@ -2,19 +2,32 @@ require 'erb'
 require 'json'
 
 filename = 'testInput.json'
-#out_name = 'TestSlides'
+out_name = 'TestSlides'
 
 #read the questions JSON and create the hash
-questions = JSON.parse(File.read('./src/' + filename))['questions']
+questions = JSON.parse(File.read('src/' + filename))['questions']
 
-#do some processing on the questions (ie. randomize the order)
+#randomize the question order
+range = 0...questions.length
+for i in range do
+    newIndex = rand(range)
+
+    if (newIndex != i)
+        temp = questions[i]
+        questions[i] = questions[newIndex]
+        questions[newIndex] = temp
+    end
+end
 
 #compile the template
-template = ERB.new(File.read('./slidesTemplate.tex.erb'))
+template = ERB.new(File.read('slidesTemplate.tex.erb'))
 
 #write to the file
-open('bin/slides.tex', 'w') do |f|
+outTexFile = 'bin/slides.tex'
+open(outTexFile, 'w') do |f|
     f.puts template.result
 end
 
 #run the latex compiler
+#TODO: fix additional flags
+system("/usr/local/texlive/2019/bin/x86_64-darwin/pdflatex #{outTexFile} --output-directory=output/ -job-name=#{out_name} -aux-directory=bin/")
